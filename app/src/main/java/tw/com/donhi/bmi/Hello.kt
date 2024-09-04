c3ef0e180ce05064c968067e02674686628f4e2a
Tetsu <ttgchang@gmail.com>
2024-09-04 13:08:24 +0800

    Hello Person Class

--- app/src/main/java/tw/com/donhi/bmi/Hello.kt
+++ app/src/main/java/tw/com/donhi/bmi/Hello.kt
@@ -5,7 +5,11 @@ class Hello {
 }
 
 fun main() {
+    val p = Person() //記憶體內建立物件 p 的住址
+    p.hello()
+
     println("Hello Kotlin!")
+
     //Byte(8bits), Short(16), Int(32), Long(64)
     val age:Int = 20 //val 定義不可變動的變數，可省略 :Int 由程式自動判斷
     val population:Long = 9999L //L 代表 Long 故可省略定義 :Long
---
+++ app/src/main/java/tw/com/donhi/bmi/Person.kt
@@ -0,0 +1,8 @@
+package tw.com.donhi.bmi
+
+class Person {
+    // ()內可加傳入值
+    fun hello() {
+        println("Hello")
+    }
+}
