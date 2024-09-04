b4e753e8ca74af20343ca268ba0a8319a9fba8e7
Tetsu <ttgchang@gmail.com>
2024-09-04 16:44:36 +0800

    add Random

--- app/src/main/java/tw/com/donhi/bmi/Guessing.kt
+++ app/src/main/java/tw/com/donhi/bmi/Guessing.kt
@@ -1,13 +1,24 @@
 package tw.com.donhi.bmi
 
+import kotlin.random.Random
+
 //猜數字
 fun main() {
-    var secret = 7
-    var num = 0
+    //產生亂數
+    //Random 內涵static方法 可直接使用的類別
+    // .next...回傳各類型值
+    //println(Random.nextInt(from = 1, until = 11))
+    //使用範圍
+    //println((1..10).random())
 
+    var secret = (1..10).random()
+    println("secret: $secret")
+    var num = 0
     //重複執行程式碼 while 迴圈
     //不建議使用 while(true)，建議使用判斷式
-    while (num != secret) {
+//    while (num != secret) {
+    var bingo = false
+    for (i in 1..3) {
         //可直接執行者，即為 第一階層的方法(function)
         print("請輸入一個數字(1-10): ")
         //在 commend Line輸入數字
@@ -27,8 +38,12 @@ fun main() {
             println("再小一點!")
         } else {
             println("你猜對了!")
-
+            bingo = true
+            break
         }
     }
 
+    if (!bingo) {
+        println("你失敗了，數字為: $secret")
+    }
 }
