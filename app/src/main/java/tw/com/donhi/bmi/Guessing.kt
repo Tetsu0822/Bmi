adb0bbddf4f1c6a9fc736fde4c90e63b909a44d5
Tetsu <ttgchang@gmail.com>
2024-09-04 15:43:59 +0800

    add if..else..

--- app/src/main/java/tw/com/donhi/bmi/Guessing.kt
+++ app/src/main/java/tw/com/donhi/bmi/Guessing.kt
@@ -2,6 +2,7 @@ package tw.com.donhi.bmi
 
 //猜數字
 fun main() {
+    var secret = 7
     //可直接執行者，即為 第一階層的方法(function)
     print("請輸入一個數字(1-10): ")
     //在 commend Line輸入數字
@@ -12,5 +13,14 @@ fun main() {
     var num:Int = input?.toIntOrNull() ?: 0
     println("你輸入的值為: " + num)
     //將 結果崁入字串，若結果是運算得來的，則要在字串內使用大括弧{}包覆
-    println("你輸入的值為: $num ${input?.length}")
+    //println("你輸入的值為: $num ${input?.length}")
+
+    // if..else..判斷式
+    if ( num < secret ) {
+        println("再大一點!")
+    } else if ( num > secret ) {
+        println("再小一點!")
+    } else {
+        println("你猜對了!")
+    }
 }
