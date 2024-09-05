114f3eaa34397e320d1d3caa678954ba73423e70
Tetsu <ttgchang@gmail.com>
2024-09-05 17:28:23 +0800

    學生案例結束

--- app/src/main/java/tw/com/donhi/bmi/student/Student.kt
+++ app/src/main/java/tw/com/donhi/bmi/student/Student.kt
@@ -7,22 +7,61 @@ package tw.com.donhi.bmi.student
 
 //設計藍圖
 //類別(屬性)只能在第一層定義，其他均為區域變數 Local variable
+//主建構子可預先設定值
 class Student(val id: String, val name: String,
-                var english: Int, var math: Int) {
+                var english: Int = 0, var math: Int = 0) {
 
     //次要建構子是為了讓類別在某些情境上的使用，更能夠符合他們的需求
     //冒號後為回傳至主要建構子
-    constructor(id: String, name: String) : this(id,name,0,0)
+//    constructor(id: String, name: String) : this(id,name,0,0)
     fun print() {
         // /t 為定位點即空白
-        println("$id\t$name\t$english\t$math")
+        println("$id\t$name\t$english\t$math\t${grading()}")
     }
+    //簡化成下面 fun
+    /*fun average() : Int {
+        return (english + math)/2
+    }*/
+    fun average() = (english + math)/2
+
+    fun grading() = when(average()/10) {
+        9,10 -> "A"
+        8    -> "B"
+        7    -> "C"
+        6    -> "D"
+        else -> "F"
+    }
+    //簡化成上面 fun
+    /*fun grading() : String {
+        return when(average()/10) {
+            9,10 -> "A"
+            8    -> "B"
+            7    -> "C"
+            6    -> "D"
+            else -> "F"
+        }
+    }*/
+
 }
 
 //演練實作程式
 fun main() {
-    var stu1 = Student("001", "Jack", 70, 80)
-    var stu2 = Student("002", "Tetsu", 80, 90)
+    //利用集合印出學生資料
+    var students = listOf<Student>(
+        Student("001", "Jack", 100, 100),
+        Student("002", "Tetsu", 95, 99),
+        Student("003", "Jane")
+    )
+    for (i in 0..2) {
+        students.get(i).print()
+    }
+    //直接讓集合列出每一個資料
+    for (stu in students) {
+        stu.print()
+    }
+
+    var stu1 = Student("001", "Jack", 100, 100)
+    var stu2 = Student("002", "Tetsu", 95, 99)
     var stu3 = Student("003", "Jane")
     stu1.print()
     stu2.print()
