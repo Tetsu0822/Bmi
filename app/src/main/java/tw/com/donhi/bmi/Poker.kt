77c822288d0700d986c667e1d4b32289c44049e6
Tetsu <ttgchang@gmail.com>
2024-09-05 16:05:43 +0800

    Set 集合, shuffle(), shuffled()

--- app/src/main/java/tw/com/donhi/bmi/Poker.kt
+++ app/src/main/java/tw/com/donhi/bmi/Poker.kt
@@ -1,8 +1,25 @@
 package tw.com.donhi.bmi
 
 fun main() {
+    //Set 集合:不重複資料，重複的資料將被移除
+    //特性:資料沒有順序(無索引值 index)
+    val set = setOf<Int>(5,3,9,3,2,1,9)
+    println(set)
+    //列出每一個值(無命名)
+    set.forEach {
+        println(it)
+    }
+    //有命名
+    set.forEach { num ->
+        println(num)
+    }
     //C, D, H, S
+    //List 特性1:有索引值 index
+    //List 特性2:值可重複
+    val list = listOf<Int>(5,3,8,5,3,2,1)
+    println(list)
     var deck = mutableListOf<Int>()
+
     for (i in 0 until 52) { //until 直到小余..
         deck.add(i)
         if (i%13 == 0 && i != 0) println()
@@ -57,8 +74,16 @@ fun main() {
 //        } else if (a == 2) {
 //            type = "H"
 //        }
-
-
-
     }
+    println()
+    println(deck)
+    //若集合為mutableList 則可使用shuffle 打亂順序
+    deck.shuffle()
+    //打亂順序並產生第 2 個集合
+    val deck2 = deck.shuffled()
+    println("deck: $deck")
+    println("deck2: $deck2")
+    // 值有可能重複
+    val a = deck.shuffled().first()
+    val b = deck.shuffled().first()
 }
