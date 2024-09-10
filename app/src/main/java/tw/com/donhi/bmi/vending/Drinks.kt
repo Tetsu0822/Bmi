f273dc25d47043c96b8e5a97033472af54c161d4
TetsugoChang <ttgchang@gmail.com>
2024-09-10 20:50:04 +0800

    擴充功能 extensions

--- app/src/main/java/tw/com/donhi/bmi/vending/Drinks.kt
+++ app/src/main/java/tw/com/donhi/bmi/vending/Drinks.kt
@@ -3,8 +3,12 @@ package tw.com.donhi.bmi.vending
 //Map 一組資料:key-Value
 //Key:不可重複, Value:可重複
 fun main() {
-    var tea = Drink("Black tea", 0, 50)
+    val s = "abcdf"
+    println(s.validate())
+    val tea = Drink("Black tea", 0, 50)
+    val discountTea = tea.off20()
     println(tea)
+    println(discountTea)
     //複製舊物件以產生新物件並可微調
     val tea2 = tea.copy(suger = 5)
     println(tea2)
@@ -19,4 +23,13 @@ fun main() {
     println(drinks.get(3))
     drinks.put(5, "water")
     println(drinks)
+}
+
+//擴充功能 extensions
+//回傳新的物件
+fun Drink.off20() : Drink {
+    return this.copy(price = (this.price * 0.8).toInt())
+}
+fun String.validate() : Boolean {
+    return this.length >= 6
 }
