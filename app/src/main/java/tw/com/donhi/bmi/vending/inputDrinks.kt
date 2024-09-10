c73b32c68f70641f7eea9ed1b9e919d077751a0f
TetsugoChang <ttgchang@gmail.com>
2024-09-10 22:04:03 +0800

    Exception 例外, try..catch..

--- app/src/main/java/tw/com/donhi/bmi/vending/inputDrinks.kt
+++ app/src/main/java/tw/com/donhi/bmi/vending/inputDrinks.kt
@@ -11,11 +11,20 @@ fun main() {
     while (line != null) {
         println(line)
         //Black Tea,0,50
+
         val tokens = line.split(",") //將字串拆解成區塊,切割字元為逗號
-        //產生陣列
-        val drink = Drink(tokens[0], tokens[1].toInt(),
-            tokens[2].toInt())
-        drinks.add(drink) //加入一行資料
+        //Exception 監控區塊
+        try {
+            //產生陣列
+            val drink = Drink(tokens[0], tokens[1].toInt(),
+                tokens[2].toInt())
+            drinks.add(drink) //加入一行資料
+        }
+        //Exception 處理區塊, 1個try至少配1個catch
+        //catch (ex:Exception) 也可只指定例外主類別
+        catch (ex:IndexOutOfBoundsException) {
+            println("Invalid line data format!")
+        }
         line = input.readLine() //迴圈讀取下一行
     }
     println(drinks) //印出物件
