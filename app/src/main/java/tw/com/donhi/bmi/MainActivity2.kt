3d0375ec15a0ebd71d8f76a9a008486272d4a6a7
TetsugoChang <ttgchang@gmail.com>
2024-09-13 00:25:36 +0800

    Toast 訊息框

--- app/src/main/java/tw/com/donhi/bmi/MainActivity2.kt
+++ app/src/main/java/tw/com/donhi/bmi/MainActivity2.kt
@@ -3,6 +3,7 @@ package tw.com.donhi.bmi
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,8 @@ import tw.com.donhi.bmi.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    val serect = (1..10).random()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,11 +25,17 @@ class MainActivity2 : AppCompatActivity() {
         }
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Toast.makeText(this, "serect: $serect", Toast.LENGTH_LONG).show()
     }
     fun guess(view: View) {
         if (!binding.number.text.toString().equals("")) {
             val num = binding.number.text.toString().toInt()
             Log.d("MainActivity2", num.toString())
+        } else {
+            //顯示訊息框(Context=MainActivity,String=訊息,Length 長度)
+            //Toast 顯示不重要的提示訊息
+            Toast.makeText(this, "Please Enter a Number",
+                Toast.LENGTH_LONG).show()
         }
     }
 }
