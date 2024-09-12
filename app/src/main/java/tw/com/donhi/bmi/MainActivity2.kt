59ba58d8c95a83d2121021386925ea9c748a976f
Tetsu <ttgchang@gmail.com>
2024-09-12 18:00:47 +0800

    用ViewBinding連接Activity,Layout,元件執行function

---
+++ app/src/main/java/tw/com/donhi/bmi/MainActivity2.kt
@@ -0,0 +1,28 @@
+package tw.com.donhi.bmi
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import tw.com.donhi.bmi.databinding.ActivityMainBinding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    fun guess(view: View) {
+        println("Clicked")
+    }
+}
