2965371b109442b09931d07e23242c0e1525a0e9
Tetsu <ttgchang@gmail.com>
2024-09-13 15:27:06 +0800

    Compose 宣告式的設計元件

--- app/src/main/java/tw/com/donhi/bmi/MainActivity.kt
+++ app/src/main/java/tw/com/donhi/bmi/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.android.material.search.SearchBar
 import tw.com.donhi.bmi.ui.theme.BmiTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +31,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             BmiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                    SearchBar(
+                        modifier = Modifier.padding(
+                            top = 100.dp
+                        )
                     )
                 }
             }
@@ -32,16 +48,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box(modifier = modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text("Text2")
+        Text("Text3 Testing")
+    }
+}
+
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    Column {
+        TextField(
+            value = "",
+            onValueChange = {},
+            placeholder = {
+                Text(text = "Search")
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "")
+            },
+            modifier = modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyPreview() {
     BmiTheme {
-        Greeting("Android")
+        SearchBar()
     }
 }
