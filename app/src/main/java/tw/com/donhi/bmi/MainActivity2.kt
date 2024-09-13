ad8ff9ae1bd4dd3c691609551410cd737678af4f
Tetsu <ttgchang@gmail.com>
2024-09-13 11:03:26 +0800

    AlertDialog 對話框

--- app/src/main/java/tw/com/donhi/bmi/MainActivity2.kt
+++ app/src/main/java/tw/com/donhi/bmi/MainActivity2.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -31,6 +32,19 @@ class MainActivity2 : AppCompatActivity() {
         if (!binding.number.text.toString().equals("")) {
             val num = binding.number.text.toString().toInt()
             Log.d("MainActivity2", num.toString())
+            val message = if (num > serect) {
+                "Smaller"
+            } else if (num < serect) {
+                "Bigger"
+            } else {
+                "You got it!"
+            }
+            //建置對話框
+            AlertDialog.Builder(this)
+                .setTitle("Info")
+                .setMessage(message)
+                .setPositiveButton("OK", null)
+                .show()
         } else {
             //顯示訊息框(Context=MainActivity,String=訊息,Length 長度)
             //Toast 顯示不重要的提示訊息
